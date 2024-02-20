2c27329f71e5bef59e0442e74999d8359ba85129
wybin94 <wyb@iquest.kr>
2024-02-20 14:30:36 +0900

    commit1

--- src/main/java/com/test/springmvc/common/HelloController.java
+++ src/main/java/com/test/springmvc/common/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String hi(){
 		ModelAndView mav = new ModelAndView();
+		mav.addObject("result", true);
 		mav.setViewName("home");
 		return "home";
 	}
